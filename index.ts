a65a7b62b62b4261c4769152db5df5d6319d110a
EnderDev <42723993+EnderDev@users.noreply.github.com>
2022-01-03 19:41:39 +0000

    🎉 Init

---
+++ index.ts
@@ -0,0 +1,51 @@
+import { br, button, createRef, h1, render, span, _ } from "../../src";
+import anime from "animejs";
+
+const App = () => {
+    const titleRef = createRef<HTMLHeadingElement>();
+    const hexRef = createRef<HTMLSpanElement>();
+
+    const hex = () => "#" + [...Array(6)]
+        .map(() => Math.floor(Math.random() * 16).toString(16))
+        .join("");
+
+    const changeColour = () => {
+        anime({
+            targets: titleRef.current,
+            duration: 200,
+            easing: "easeOutQuad",
+            color: hex(),
+            update: (a) => hexRef.current.textContent = `Colour: ${a.animations[0].currentValue}`
+        })
+    }
+
+    window.addEventListener(
+        "DOMContentLoaded", 
+        () => changeColour()
+    );
+
+    return (
+        _( /* You can also use fragment (_ is the shorthand) */
+            h1(
+                { 
+                    ref: titleRef,
+                    style: {
+                        color: "#000000"
+                    }
+                }, 
+                "Hello World"
+            ),
+            span({ ref: hexRef }),
+            br,
+            button({ onClick: changeColour },
+                "✨ Random Colour ✨"
+            )
+        )
+    )
+}
+
+render(
+    App, 
+    document.getElementById("app")
+);
+
